5a976319ea7c8bad30a95f2633cd2de8dbd7b85e
voidShell <903458283@qq.com>
2019-05-30 16:58:41 +0800

    init

---
+++ pages/detail/detail.js
@@ -0,0 +1,27 @@
+const app = getApp()
+
+Page({
+  data: {
+    showBar:{
+      imgUrls: [
+        '../../img/987.png',
+        '../../img/IMG_3955.jpg',
+        '../../img/987.png'
+      ],
+      indicatorDots: false,
+      vertical: false,
+      autoplay: true,
+      circular: true,
+      interval: 2000,
+      duration: 500,
+      previousMargin: 0,
+      nextMargin: 0
+    },
+    envir:[
+      {}
+    ]
+  },
+  onLoad: function () {
+
+  }
+})
